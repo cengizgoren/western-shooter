b38f2e0302558460da0c181277763979e9596a7d
redmorr <19555512+redmorr@users.noreply.github.com>
2022-10-01 19:02:06 +0200

    Improve player aim and camera calculation/customization

--- Assets/_Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/_Assets/Scripts/Player/PlayerRotation.cs
@@ -7,17 +7,17 @@ public class PlayerRotation : MonoBehaviour
     public Transform CameraRoot;
     public float RotationSpeed = 720f;
     [Space(10)]
+
     public Transform AimPoint;
-    public Transform DirectionArrow;
-    public Vector3 MousePoint;
-    [Space(10)]
-    public AnimationCurve DistanceCurve;
-    public float BaseOveraimDistance = 2f;
-    public float MaxCurveDistance = 90f;
-    public float Magnitude;
-    public float OveraimFactor;
+    public Transform DirectionArrow; 
+    
+    [Header("Camera")]
+    [Range(1.0f, 2.0f)] public float OveraimFactor = 1.25f;
+    [Range(0.0f, 1.0f)] public float MouseWeight = 0.6f;
+    [Range(0.0f, 100.0f)] public float MaxCameraOffset = 25f;
 
     private Plane groundPlane = new Plane(Vector3.up, new Vector3(0f, 0f, 0f));
+    private Vector3 MousePoint;
 
     private void Update()
     {
@@ -31,18 +31,16 @@ public class PlayerRotation : MonoBehaviour
         {
             MousePoint = ray.GetPoint(rayDistance);
             Vector3 playersDirectionToMouse = MousePoint - transform.position;
-            
+
             // Camera
-            CameraRoot.position = transform.position + (MousePoint - transform.position) / 5;
-            
+            CameraRoot.position = transform.position + Vector3.ClampMagnitude(MouseWeight * playersDirectionToMouse, MaxCameraOffset);
+
             // Rotation
             Quaternion targetRotation = Quaternion.LookRotation(new Vector3(playersDirectionToMouse.x, 0f, playersDirectionToMouse.z), Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * RotationSpeed);
 
             // Aim Point
-            Magnitude = playersDirectionToMouse.magnitude;
-            OveraimFactor = DistanceCurve.Evaluate(Magnitude / MaxCurveDistance);
-            AimPoint.position = MousePoint + OveraimFactor * BaseOveraimDistance * new Vector3(playersDirectionToMouse.x, 0f, playersDirectionToMouse.z).normalized;
+            AimPoint.position = transform.position + OveraimFactor * playersDirectionToMouse;
 
             // Direction Arrow
             DirectionArrow.position = new Vector3(MousePoint.x, DirectionArrow.position.y, MousePoint.z);
