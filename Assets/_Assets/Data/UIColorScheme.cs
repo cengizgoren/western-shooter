494ef631a36dfcc1d07f7293a08fdaa2586ed34a
redmorr <19555512+redmorr@users.noreply.github.com>
2022-10-10 18:12:53 +0200

    Implement concept of UI objectives

---
+++ Assets/_Assets/Data/UIColorScheme.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "UI Color Scheme", fileName = "UIColorScheme")]
+public class UIColorScheme : ScriptableObject
+{
+    public Color ObjectivesBackground;
+}
