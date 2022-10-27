c8d78dbb23c035529353dc0446e7c78e3a766a6f
redmorr <19555512+redmorr@users.noreply.github.com>
2022-10-27 18:23:58 +0200

    Minor level fixes, move editor code out of game class

---
+++ Assets/_Assets/Scripts/Enemy/StateMachine/DebugItem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class DebugItem
+{
+    public bool Show = false;
+    public Color Color = Color.cyan;
+}
--- Assets/_Assets/Scripts/Enemy/StateMachine/TargetDetector.cs
+++ Assets/_Assets/Scripts/Enemy/StateMachine/TargetDetector.cs
@@ -1,8 +1,6 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 
-public class TargetDetector : MonoBehaviour
+public partial class TargetDetector : MonoBehaviour
 {
     public bool TargetSighted = false;
     public bool TargetObstructed = false;
@@ -109,44 +107,6 @@ public class TargetDetector : MonoBehaviour
         return false;
     }
 
-    [Serializable]
-    public class DebugItem
-    {
-        public bool Show = false;
-        public Color Color = Color.cyan;
-    }
-
-    [CustomPropertyDrawer(typeof(DebugItem))]
-    public class IngredientDrawerUIE : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            // Using BeginProperty / EndProperty on the parent property means that
-            // prefab override logic works on the entire property.
-            EditorGUI.BeginProperty(position, label, property);
-
-            // Draw label
-            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-            // Don't make child fields be indented
-            var indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 0;
-
-            // Calculate rects
-            var amountRect = new Rect(position.x, position.y, 30, position.height);
-            var unitRect = new Rect(position.x + 35, position.y, 50, position.height);
-
-            // Draw fields - pass GUIContent.none to each so they are drawn without labels
-            EditorGUI.PropertyField(amountRect, property.FindPropertyRelative("Show"), GUIContent.none);
-            EditorGUI.PropertyField(unitRect, property.FindPropertyRelative("Color"), GUIContent.none);
-
-            // Set indent back to what it was
-            EditorGUI.indentLevel = indent;
-
-            EditorGUI.EndProperty();
-        }
-    }
-
     private void OnDrawGizmos()
     {
         //if (isThereLastKnownPos)
@@ -183,6 +143,4 @@ public class TargetDetector : MonoBehaviour
             Gizmos.DrawLine(transform.position, transform.position + toPlayer);
         }
     }
-
-
 }
