184dc63c174ba0f59e605bd9aa006965ac885d82
redmorr <19555512+redmorr@users.noreply.github.com>
2022-11-01 16:57:00 +0100

    Player rotatates whole body towards aimpoint instead of hands being there faster

--- Assets/_Assets/Scripts/Player/PlayerRotation.cs
+++ Assets/_Assets/Scripts/Player/PlayerRotation.cs
@@ -1,28 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRotation : MonoBehaviour
 {
-    public Vector2 pos;
+    public float AngularSpeed = 720f;
     public Transform CameraRoot;
-    public float RotationSpeed = 720f;
-    [Space(10)]
-
     public Transform AimPoint;
-    public Transform DirectionArrow; 
-    
-    [Header("Camera")]
+    public Transform DirectionArrow;
+
+    [Space(10)]
     [Range(1.0f, 5.0f)] public float OveraimFactor = 1.25f;
     [Range(0.0f, 1.0f)] public float MouseWeight = 0.6f;
     [Range(0.0f, 100.0f)] public float MaxCameraOffset = 25f;
 
     private Plane groundPlane = new Plane(Vector3.up, new Vector3(0f, 0f, 0f));
+    private Vector3 forwardAim = new Vector3(0f, 0f, 0f);
+    private Vector2 pos;
     private Vector3 MousePoint;
 
     private void Update()
     {
         Rotation();
+    }
+
+    private void Awake()
+    {
+        AimPoint.localPosition = Vector3.zero;
     }
 
     private void Rotation()
@@ -41,13 +43,15 @@ public class PlayerRotation : MonoBehaviour
 
                 // Rotation
                 Quaternion targetRotation = Quaternion.LookRotation(new Vector3(playersDirectionToMouse.x, 0f, playersDirectionToMouse.z), Vector3.up);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * RotationSpeed);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * AngularSpeed);
 
                 // Aim Point
-                AimPoint.position = transform.position + OveraimFactor * playersDirectionToMouse;
-            
+                forwardAim.z = playersDirectionToMouse.magnitude * OveraimFactor;
+                AimPoint.localPosition = forwardAim;
+
                 // Direction Arrow
                 DirectionArrow.position = new Vector3(MousePoint.x, DirectionArrow.position.y, MousePoint.z);
+                DirectionArrow.rotation = Quaternion.LookRotation(playersDirectionToMouse);
             }
         }
     }
