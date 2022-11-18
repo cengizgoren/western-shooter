0ae9f90f506046b0f0a930abe4ca3d4c129296c8
redmorr <19555512+redmorr@users.noreply.github.com>
2022-11-18 20:33:08 +0100

    Make AI Commander tick one enemy AI per frame

--- Assets/_Assets/Scripts/Enemy/AI/AICommander.cs
+++ Assets/_Assets/Scripts/Enemy/AI/AICommander.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
@@ -11,25 +12,35 @@ public class AICommander : MonoBehaviour
     private const int AVOIDANCE_PRIORITY_RANGE = 100;
     public List<EnemyStateMachine> enemyStateMachines;
 
+    private int i;
+
     private void Awake()
     {
         int i = 0;
         enemyStateMachines = new List<EnemyStateMachine>(FindObjectsOfType<EnemyStateMachine>());
-        foreach(var e in enemyStateMachines)
+        foreach (var e in enemyStateMachines)
         {
             e.GetComponent<NavMeshAgent>().avoidancePriority = i % AVOIDANCE_PRIORITY_RANGE;
             i++;
         }
     }
 
+    private void Start()
+    {
+        i = 0;
+    }
+
     private void Update()
     {
-        foreach (var e in enemyStateMachines)
-        {
-            if (e)
-            {
-                e.Tick();
-            }
-        }
+        if (i >= enemyStateMachines.Count)
+            i = 0;
+
+        // TODO: Account for null elements skipped
+        // TODO: Make more roboust solution for a list of references to enemies
+
+        if (enemyStateMachines[i])
+            enemyStateMachines[i].Tick();
+
+        i++;
     }
 }
